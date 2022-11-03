69781ffef079b684e77d212ac7c303043f4311c2
thankrain <1573856599@qq.com>
2022-11-03 16:46:12 +0800

    Initial Commit

---
+++ MyTest/MyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTestApp.swift
+//  MyTest
+//
+//  Created by Xeu on 2022/11/3.
+//
+
+import SwiftUI
+
+@main
+struct MyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
